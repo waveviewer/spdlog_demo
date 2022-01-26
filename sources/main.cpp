1bff7b90a5d527fafa86429b3de59ab56a1ef20d
wave <waveviewer@foxmail.com>
2022-01-26 10:04:23 +0800

    first commit

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,6 +1,12 @@
 //
 // Created by wave on 2022/1/26.
 //
+#include <iostream>
+#include "spdlog/spdlog.h"
+using namespace std;
 
-
+int main(){
+  spdlog::info("First log info");
+  return 0;
+}
 
